544f88e1a5fecce18f123c5100a143acc47f6061
Joep Joosten <joepjoosten@gmail.com>
2024-12-05 09:46:43 +0100

    Small fixes, prettierrc

--- 2024/day/1/answer.ts
+++ 2024/day/1/answer.ts
@@ -1,52 +1,57 @@
 import * as Syntax from "@effect/parser/Syntax";
 import { Array, Chunk, Effect, Either, flow, HashMap, Option, Order, pipe, Tuple } from "effect";
-import { toInteger } from "../../../utils/parser.js";
+import { repeatedChar, toInteger } from "../../../utils/parser.js";
 import { distance, histogram, sumArray, sumChunk } from "../../../utils/utils.js";
 
-const seperator = pipe(Syntax.char(' '), Syntax.zipRight(Syntax.char(' ')), Syntax.zipRight(Syntax.char(' ')));
-const newline = Syntax.char('\n');
+const seperator = repeatedChar(" ", 3);
+const newline = Syntax.char("\n");
 const integer = pipe(Syntax.digit, Syntax.repeat1, Syntax.captureString, toInteger);
 const line = pipe(integer, Syntax.repeatWithSeparator1(seperator));
-export const grammer = pipe(line, Syntax.repeatWithSeparator1(newline))
+export const grammer = pipe(line, Syntax.repeatWithSeparator1(newline));
 
-export const part1 = (input: string) => Effect.gen(function* () {
-    return yield* pipe(
-        Syntax.parseString(grammer, input),
-        Either.map(flow(
-            Chunk.flatten,
-            Chunk.partition((_, i) => i % 2 === 0),
-            Tuple.mapBoth({
-                onFirst: flow(Chunk.sort(Order.number)),
-                onSecond: flow(Chunk.sort(Order.number)),
-            }),
-            ([right, left]) => Chunk.zip(right, left),
-            Chunk.map(distance),
-            sumChunk
-        ))
-    );
-});
+const shaping = (input: string) =>
+  pipe(
+    Syntax.parseString(grammer, input),
+    Either.getOrThrow,
+    Chunk.flatten,
+    Chunk.partition((_, i) => i % 2 === 0)
+  );
 
-export const part2 = (input: string) => Effect.gen(function* () {
-    return yield* pipe(
-        Syntax.parseString(grammer, input),
-        Either.map(flow(
-            Chunk.flatten,
-            Chunk.partition((_, i) => i % 2 === 0),
-            Tuple.mapBoth({
-                onFirst: (chunk) => histogram(chunk, Order.number),
-                onSecond: (chunk) => histogram(chunk, Order.number),
-            }),
-            ([right, left]) => pipe(
-                HashMap.toEntries(left), 
-                Array.map(([k, v]) => pipe(
-                    HashMap.get(right, k), 
-                    Option.match({ 
-                        onNone: () => 0, 
-                        onSome: (y) => k * v * y })
-                    )
-                )
-            ),
-            sumArray
-        ))
+export const part1 = (input: string) =>
+  Effect.gen(function* () {
+    return pipe(
+      shaping(input),
+      Tuple.mapBoth({
+        onFirst: flow(Chunk.sort(Order.number)),
+        onSecond: flow(Chunk.sort(Order.number)),
+      }),
+      ([right, left]) => Chunk.zip(right, left),
+      Chunk.map(distance),
+      sumChunk
     );
-});
+  });
+
+export const part2 = (input: string) =>
+  Effect.gen(function* () {
+    return pipe(
+      shaping(input),
+      Tuple.mapBoth({
+        onFirst: (chunk) => histogram(chunk, Order.number),
+        onSecond: (chunk) => histogram(chunk, Order.number),
+      }),
+      ([right, left]) =>
+        pipe(
+          HashMap.toEntries(left),
+          Array.map(([k, v]) =>
+            pipe(
+              HashMap.get(right, k),
+              Option.match({
+                onNone: () => 0,
+                onSome: (y) => k * v * y,
+              })
+            )
+          )
+        ),
+      sumArray
+    );
+  });
--- utils/parser.ts
+++ utils/parser.ts
@@ -1,5 +1,28 @@
 import { Chunk } from "effect";
 import * as Syntax from "@effect/parser/Syntax";
+import { char } from "@effect/parser/Regex";
 
-export const toArray = <T>() => Syntax.transform<Chunk.NonEmptyChunk<T>, [T, ...T[]]>(Chunk.toArray, Chunk.unsafeFromNonEmptyArray);
-export const toInteger = Syntax.transform<string, number>((x) => parseInt(x), String)
+const repeatChar = (
+  char: string,
+  charSyntax: Syntax.Syntax<string, Error, string, void>,
+  times: number
+): Syntax.Syntax<string, Error, string, void> =>
+  times === 0
+    ? charSyntax
+    : repeatChar(
+        char,
+        Syntax.zipRight(Syntax.char(char), charSyntax),
+        times - 1
+      );
+export const repeatedChar = (char: string, times: number) =>
+  repeatChar(char, Syntax.char(char), times - 1);
+
+export const toArray = <T>() =>
+  Syntax.transform<Chunk.NonEmptyChunk<T>, [T, ...T[]]>(
+    Chunk.toArray,
+    Chunk.unsafeFromNonEmptyArray
+  );
+export const toInteger = Syntax.transform<string, number>(
+  (x) => parseInt(x),
+  String
+);
