fac1fa526bb1a3460ae048b4800b8e664e60b954
Joep Joosten <joepjoosten@gmail.com>
2024-12-05 10:16:17 +0100

    Updated to template

--- 2024/day/1/answer.spec.ts
+++ 2024/day/1/answer.spec.ts
@@ -6,15 +6,17 @@ import { Effect, Either } from "effect";
 import { describe, expect } from "vitest";
 import { grammer, part1, part2 } from "./answer.js";
 
-describe('Does the parser work?', () => {
+describe('year 2024 - day 1 - does the parser work?', () => {
     effect('should return the correct answer', () => Effect.gen(function* () {
         const fs = yield* FileSystem.FileSystem;
         const input = yield* fs.readFileString('./2024/day/1/snippet-1.txt');
         const parsedAndPrinted = Syntax.printString(grammer, Either.getOrThrow(Syntax.parseString(grammer, input)));
         expect(Either.getOrThrow(parsedAndPrinted) + "\n").toEqual(input);
     }).pipe(Effect.provide(NodeContext.layer)));
+});
 
-    effect('part 1 - snippet 1', () => Effect.gen(function* () {
+describe('year 2024 - day 2 - are the examples working?', () => {
+        effect('part 1 - snippet 1', () => Effect.gen(function* () {
         const fs = yield* FileSystem.FileSystem;
         const snippet = yield* fs.readFileString('./2024/day/1/snippet-1.txt');
         const result = yield* part1(snippet);
@@ -27,5 +29,4 @@ describe('Does the parser work?', () => {
         const result = yield* part2(snippet);
         expect(result).toEqual(31);
     }).pipe(Effect.provide(NodeContext.layer)));
-
-})
+});
