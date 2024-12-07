c4d8f5b6b4d8776b304aaf92f8040b054e59f413
Joep Joosten <joepjoosten@gmail.com>
2024-12-07 08:49:48 +0100

    WIP Array

--- utils/Array.ts
+++ utils/Array.ts
@@ -1,4 +1,4 @@
-import { Array } from "effect";
+import { Array, pipe } from "effect";
 import { invoke } from "./Function.js";
 
 export const slice =
@@ -16,3 +16,27 @@ export const aperture =
 
     return n < 1 ? [] : go(0)([]);
   };
+
+export const transpose = <T>(xs: Array<Array<T>>): Array<Array<T>> =>
+  xs.length === 0 ? [] : pipe(xs[0], Array.map((_, i) => Array.map((ys) => ys[i])(xs)));
+
+export const getHorizontalFromRectangle = <T>(input: Array<Array<T>>) => input;
+export const getVerticalsFromRectangle = <T>(input: Array<Array<T>>) => input[0].map((_, i) => input.map((row) => row[i]));
+
+export const getLowerLeftDiagonals = <T>(input: Array<Array<T>>): Array<Array<T>> =>
+  pipe(
+    Array.range(0, input.length - 1),
+    Array.map(i =>
+      pipe(
+        Array.range(0, Math.min(input[0].length, input.length - i) - 1),
+        Array.map(j => input.at(i + j).at(j))
+      )
+    )
+  );
+
+export const getDiagonals = <T>(input:  Array<Array<T>>) => [
+  ...getLowerLeftDiagonals(input),
+  ...getLowerLeftDiagonals(transpose(input)).slice(1),
+  ...getLowerLeftDiagonals(input.map((row) => row.reverse())),
+  ...getLowerLeftDiagonals(transpose(input.map((row) => row.reverse()))).slice(1),
+]
