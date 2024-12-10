6a560bd7fad1b9cbb3f511902c6af012f12f6115
Joep Joosten <joepjoosten@gmail.com>
2024-12-10 19:28:39 +0100

    WIP

--- 2024/day/7/answer.ts
+++ 2024/day/7/answer.ts
@@ -1,7 +1,10 @@
 import * as Syntax from "@effect/parser/Syntax";
 import { Effect, pipe } from "effect";
+import { integer } from "../../../utils/parser";
 
-export const grammer = pipe(Syntax.anyString, Syntax.repeatWithSeparator1(Syntax.char("\n")));
+export const newline = Syntax.char("\n");
+export const line = pipe(integer, Syntax.zip(pipe(Syntax.zipRight(Syntax.char(":"), Syntax.char(" ")))), Syntax.zip(pipe(integer, Syntax.repeatWithSeparator1(Syntax.char(' ')))));
+export const grammer = pipe(line, Syntax.repeatWithSeparator1(newline));
 
 export const part1 = (input: string) =>
   Effect.gen(function* () {
