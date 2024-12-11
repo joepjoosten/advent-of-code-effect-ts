27e999f37577a7209b5fabd304d2fd0246d17c1f
Joep Joosten <joepjoosten@gmail.com>
2024-12-11 20:08:10 +0100

    WIP day 10

--- 2024/day/10/answer.ts
+++ 2024/day/10/answer.ts
@@ -1,6 +1,8 @@
 import * as Syntax from "@effect/parser/Syntax";
 import { Effect, pipe } from "effect";
+import { toInteger } from "../../../utils/parser";
 
+const height = pipe(Syntax.digit, toInteger);
 export const grammer = pipe(Syntax.anyString, Syntax.repeatWithSeparator1(Syntax.char("\n")));
 
 export const part1 = (input: string) =>
